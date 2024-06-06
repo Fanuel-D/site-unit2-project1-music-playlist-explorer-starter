6533cde96a215a6429a0447d2a87b9b60a226e48
Fanuel Dana <fanueld@meta.com>
2024-06-06 07:41:06 -0700

    Population Complete

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,7 +1,6 @@
 // JavaScript for Opening and Closing the Modal
 var modal = document.getElementById("festivalModal");
 var span = document.getElementsByClassName("close")[0];
-let count = 0;
 let bottomMod = document.querySelector(".bottomMod") 
 let clone1 = bottomMod.cloneNode(true)
 
@@ -17,37 +16,21 @@ function openModal(festival,event) {
          event.target.parentNode.querySelector(".close").onclick = function() {
             event.target.parentNode.querySelector(".modal").style.display = "none"
          }
-         modal1.querySelector(".festivalName").innerText = festival.playlist_name;
-         modal1.querySelector(".festivalImage").src = festival.playlist_art;
-         modal1.querySelector(".festivalText").innerText = festival.playlist_creator;
-         let temp3 = modal1.querySelector(".bottomPart")
-         modal1.querySelector("h3").innerText = songs[0].title
-         if (count == 0){
-            for(let i = 1; i < songs.length; i++){
-               let c = clone1.cloneNode(true)
-               temp3.appendChild(c)
-               
-               c.querySelector("h3").innerText = songs[i].title
-   
-            }
-         }
-         
          
          // window.onclick = function() {
          //    event.target.parentNode.querySelector(".modal").style.display = "none"
          // }
-      }else {
-         event.target.parentElement.parentElement.querySelector(".modal").style.display = "block"
-         event.target.parentElement.parentElement.querySelector(".close").onclick = function() {
-            event.target.parentElement.parentElement.querySelector(".modal").style.display = "none"
-         }
-         // window.onclick = function() {
-         //    event.target.parentElement.parentElement.querySelector(".modal").style.display = "none"
-         // }
       }
+      // else {
+      //    event.target.parentElement.parentElement.querySelector(".modal").style.display = "block"
+      //    event.target.parentElement.parentElement.querySelector(".close").onclick = function() {
+      //       event.target.parentElement.parentElement.querySelector(".modal").style.display = "none"
+      //    }
+      //    // window.onclick = function() {
+      //    //    event.target.parentElement.parentElement.querySelector(".modal").style.display = "none"
+      //    // }
+      // }
    
-   
-   count+=1
 }
 
 span.onclick = function() {
@@ -70,29 +53,44 @@ let playlists = data.playlists;
 
 
 playlists.forEach(function (playlist) {
+   let songs = playlist.songs;
    let temp_card = temp_card2.cloneNode(true);
    let title = temp_card.querySelector("h4")
    let image  = temp_card.querySelector("#playListImg")
    let blurb = temp_card.querySelector("p")
    blurb.innerText = `Created by ${playlist.playlist_creator}`
 
+   let mine = temp_card.querySelector(".modal")
+   mine.querySelector(".festivalName").innerText = playlist.playlist_name;
+   mine.querySelector(".festivalImage").src = playlist.playlist_art;
+   mine.querySelector(".festivalText").innerText = playlist.playlist_creator;
+   let temp3 = mine.querySelector(".bottomPart")
+   mine.querySelector("h3").innerText = songs[0].title
+   mine.querySelector("h9").innerText = songs[0].artist
+   mine.querySelector(".modImgBottom").src = songs[0].cover_art
+   mine.querySelector("h7").innerText = songs[0].album
+
+
+   for(let i = 1; i < songs.length; i++){
+         let c = clone1.cloneNode(true)
+         temp3.appendChild(c)   
+         c.querySelector("h3").innerText = songs[i].title
+         c.querySelector(".modImgBottom").src = songs[i].cover_art
+         c.querySelector("h9").innerText = songs[i].artist
+         c.querySelector("h7").innerText = songs[i].album
+   }
 
 
    image.src = playlist.playlist_art
    title.innerText = playlist.playlist_name
+   main.appendChild(temp_card)  
    temp_card.addEventListener("click", (e) => openModal(playlist,e))
-   main.appendChild(temp_card)   
+    
 })
 
 
 
 
-
-
-
-// for(let i = 0; i < 10; i++) {
-//     bottomPart.appendChild(bottomMod.cloneNode(true))
-// }
 
 
 
